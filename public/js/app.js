1b09c4a824ee386568422c96e97ec814b6d8e5a5
Md4u <anasmd4u2010@gmail.com>
2020-04-21 12:33:31 +0530

    removed unwanted console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,4 @@
 
-console.log("client side javascript loaded!")
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
 const weatherform = document.querySelector('form')
 const search      = document.querySelector('input')
 const messageOne  = document.querySelector('#message-1')
